aec4c5526ae5c6132ccf2857246f7ce6d7a73703
Shaikha Al-Emadi <shaikhaalemadi1708@gmail.com>
2025-08-31 15:57:21 +0300

    Fixed issue 3

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,6 +1,7 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+//const fibonacci = require("./fib");
+import { fibonacci } from "./fib";
 
 export default (req, res) => {
   const { num } = req.params;
